ace64979fc849e9d5c41b602a6defdb1f8cdd764
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-02-25 23:14:49 +0530

    Added comment indicating my copyright

--- bstADT.py
+++ bstADT.py
@@ -1,3 +1,23 @@
+"""
+Binary search tree implementation
+Copyright (C) 2015 Vasantha Ganesh K <vasanthaganesh.k@tuta.io>.
+
+This file is part of data-structures.
+
+data-structures is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+data-structures is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with data-structures.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 # Cool inplementation of binary search tree in python3
 class Node(object):
 	def __init__(self, data = 0):
