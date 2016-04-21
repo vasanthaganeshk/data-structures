15927274b390276bd999bd5cab53a630fd617486
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-04-21 19:14:45 +0530

    cycle detection for directed and undirected graphs (returns false if a back edge is found)

---
+++ cycle_detectection_directed.py
@@ -0,0 +1,44 @@
+parent = {}
+adj = {}
+ans = True
+def is_acyclic(u):
+    for i in adj[u]:
+        if i not in parent:
+            parent[i] = u
+            is_acyclic(i)
+        else:
+            return False
+    return True
+
+#--------------------Input-----------------------
+"""
+Input is of the form (n and then edges) :
+n
+u1 v1
+u2 v2
+u3 v3
+.
+.
+.
+un vn
+"""
+
+#--------------Driver program--------------------
+# no of edges
+n = int(input())
+
+# for directed graph
+for i in range(n):
+    key, val = map(int, input().split(' '))
+    if key not in adj:
+        adj[key] = [val]
+    else:
+        adj[key].append(val)
+
+start = int(input('start of the graph search>'))
+parent[start] = None
+
+if is_acyclic(start):
+    print("The given graph is acyclic")
+else:
+    print("The given graph is cyclic")
