51df99ae9bc477ab7c8b2bf26879e563377f1df2
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-04-28 15:43:16 +0530

    now is_acyclic() works

--- cycle_detectection_directed.py
+++ cycle_detectection_directed.py
@@ -1,15 +1,19 @@
-# cycle detectection in directed graph requires colours.
-# This code does not work
-
 parent = {}
-def is_acyclic(adj, u):
+def __is_acyclic(ans, adj, u):
+    status[u] = 'started'
     for i in adj[u]:
-        if i not in parent:
-            parent[i] = u
-            is_acyclic(adj, i)
+        if status[i] == 'not started':
+            parent[i] = None
+            return ans and __is_acyclic(ans, adj, i)
         else:
             return False
+    status[u] = 'finished'
     return True
+
+def is_acyclic(adj, u):
+    j = __is_acyclic(True, adj, u)
+    return j
+
 
 #--------------------Input-----------------------
 """
@@ -26,7 +30,7 @@ un vn
 
 #--------------Driver program--------------------
 adj = {}
-
+status = {}
 # no of edges
 n = int(input())
 
@@ -35,16 +39,18 @@ for i in range(n):
     key, val = map(int, input().split(' '))
     if key not in adj:
         adj[key] = [val]
+        status[key] = 'not started'
     else:
         adj[key].append(val)
 
     if val not in adj:
         adj[val] = []
+        status[val] = 'not started'
 
-start = int(input('start of the graph search>'))
-parent[start] = None
+initial = int(input('start of the graph search>'))
+parent[initial] = None
 
-if is_acyclic(adj, start):
+if is_acyclic(adj, initial):
     print("The given graph is acyclic")
 else:
     print("The given graph is cyclic")
